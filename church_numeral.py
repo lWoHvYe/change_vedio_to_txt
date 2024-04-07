7e7b205b4838d96a2077299997d3f5df39995450
Hongyan, Wang <39266195+lWoHvYe@users.noreply.github.com>
2024-04-07 20:00:23 +0800

    Go church_numeral.py

--- church_numeral.py
+++ church_numeral.py
@@ -3,18 +3,27 @@ def zero(f):
     return lambda x: x
 
 
-# Church numeral for add_one: lambda n: lambda f, x: f(n(f)(x))
+# Church numeral for add_one: lambda n: lambda f, x: f(n(f)(x))，其中 n(f)(x) 意为 n(f) 会返回一个函数，然后将返回的函数应用到x
 def add_one(n):
     return lambda f: lambda x: f(n(f)(x))
 
 
 # Define the successor function
 succ = lambda n: n + 1
+# succ_str = lambda n: str(n) * 2
+
+
+def succ_str(n):
+    return str(n) * 2
 
 
 # Convert Church numeral to Python integer
 def church_to_int(n):
     return n(succ)(0)
+
+
+def church_to_str(n):
+    return n(succ_str)('-')
 
 
 # Convert Python integer to Church numeral
@@ -35,3 +44,7 @@ if __name__ == '__main__':
     num = 5
     church_num = int_to_church(num)
     print("Church representation of", num, ":", church_to_int(church_num))  # Output: 5
+
+    print("Church representation of zero:", church_to_str(zero))  # Output: 0
+    print("Church representation of one:", church_to_str(add_one(zero)))  # Output: 1
+    print("Church representation of two:", church_to_str(add_one(add_one(zero))))  # Output: 2
